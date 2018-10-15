e9ee2897f599db5568a68c31b23d016ecca27cf8
Luka Devic <luka.m.devic@gmail.com>
2018-10-15 15:14:11 +0200

    initial commit

---
+++ SocialNetworkApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SocialNetworkApp.Startup))]
+namespace SocialNetworkApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
