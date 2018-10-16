673ac9c906ba50c66232b35efa8953c53bd0a3a2
Luka Devic <luka.m.devic@gmail.com>
2018-10-16 05:50:32 +0200

    Created Form for inputing Concerts

---
+++ SocialNetworkApp/Controllers/ConcertsController.cs
@@ -0,0 +1,28 @@
+﻿using SocialNetworkApp.Models;
+using SocialNetworkApp.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace SocialNetworkApp.Controllers
+{
+    public class ConcertsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ConcertsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+        public ActionResult Create()
+        {
+            var viewModel = new ConcertFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ SocialNetworkApp/ViewModels/ConcertFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SocialNetworkApp.Models;
+using System.Collections.Generic;
+
+namespace SocialNetworkApp.ViewModels
+{
+    public class ConcertFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
