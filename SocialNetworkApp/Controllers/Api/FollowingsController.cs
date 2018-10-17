f4734c1212ca550978beaac12bfddf7e99b8e5d1
Luka Devic <luka.m.devic@gmail.com>
2018-10-17 17:30:43 +0200

    Added following an artist option on the wall, throug API and JQuery

---
+++ SocialNetworkApp/Controllers/Api/FollowingsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.Identity;
+using SocialNetworkApp.Dtos;
+using SocialNetworkApp.Models;
+using System.Linq;
+using System.Web.Http;
+
+namespace SocialNetworkApp.Controllers.Api
+{
+    [Authorize]
+    public class FollowingsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public FollowingsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto dto)
+        {
+            var followerId = User.Identity.GetUserId();
+
+            if (_context.Followings.Any(f => f.FollowerId == followerId && f.FolloweeId == dto.FolloweeId))
+                return BadRequest("You allready follow this artist");
+
+
+            var following = new Following
+            {
+                FollowerId = followerId,
+                FolloweeId = dto.FolloweeId
+            };
+
+            _context.Followings.Add(following);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ SocialNetworkApp/Models/Following.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SocialNetworkApp.Models
+{
+    public class Following
+    {
+        public ApplicationUser Follower { get; set; }
+        public ApplicationUser Followee { get; set; }
+
+        [Key]
+        [Column(Order = 1)]
+        public string FollowerId { get; set; }
+
+        [Key]
+        [Column(Order = 2)]
+        public string FolloweeId { get; set; }
+    }
+}
