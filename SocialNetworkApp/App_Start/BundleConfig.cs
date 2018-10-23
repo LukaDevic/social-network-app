d41c5ea9515120c5a67c7d65cbd00a9474600351
Luka Devic <luka.m.devic@gmail.com>
2018-10-23 18:02:02 +0200

    Iproved look and feel and implemented LESS.

--- SocialNetworkApp/App_Start/BundleConfig.cs
+++ SocialNetworkApp/App_Start/BundleConfig.cs
@@ -7,9 +7,12 @@ namespace SocialNetworkApp
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
                         "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/underscore-min.js"
+                        "~/Scripts/underscore-min.js",
+                        "~/Scripts/moment.js",
+                        "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootbox.min.js"
                         ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -19,10 +22,6 @@ namespace SocialNetworkApp
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/bootbox.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
